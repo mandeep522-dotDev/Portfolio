ebfc6553f928a3b638b47b31378e587c8ef08478
mandeep kashyap <mandeepkkr140999@gmail.com>
2026-02-10 14:20:11 +0530

    Fix Netlify build: remove react-particles-js and lock node 16

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import Particle from "../Components/Particles/Particle";
+// import Particle from "../Components/Particles/Particle";
 import Techstack from "../Components/TechStack/TechStack";
 
 import Toolstack from "../Components/ToolStack/ToolStack";
@@ -10,7 +10,7 @@ import AboutCard from "../Components/AboutCard/AboutCard";
 function About() {
   return (
     <Container fluid className="about-section">
-      <Particle />
+      {/* <Particle /> */}
       <Container>
         <Row style={{ justifyContent: "center", padding: "10px" }}>
           <Col
